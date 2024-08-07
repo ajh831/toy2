520ea0c8cf534b0455e780b49dfa2122de08197f
Eseas <8snlf9@gmail.com>
2024-08-07 16:14:39 +0900

    first_commit

---
+++ toy2/src/main/java/com/fastcampus/toy2/controller/DBConnectionTestV3.java
@@ -0,0 +1,31 @@
+package com.fastcampus.toy2.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class DBConnectionTestV3 {
+    @Autowired
+    DataSource ds; // 컨테이너로부터 자동 주입받는다.
+
+    @Test
+    public void jdbcConnectionTest() throws Exception {
+//        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
+//        DataSource ds = ac.getBean(DataSource.class);
+
+        Connection conn = ds.getConnection(); // 데이터베이스의 연결을 얻는다.
+
+        System.out.println("conn = " + conn);
+        assertTrue(conn!=null);
+    }
+}
